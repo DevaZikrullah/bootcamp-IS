b1a8e5cd1650026b1c8ef8db7d5b774de3fc4b4f
DevaZikrullah <zdeva804@gmail.com>
2022-04-16 14:36:40 +0700

    vue

--- resources/js/components/ToDoComponent.vue
+++ resources/js/components/ToDoComponent.vue
@@ -2,12 +2,12 @@
     <div class="container">
         <h1>TODO</h1>
         <form class="form">
-            <input v-model="input">
-            <button type="button">Submit</button>
+            <input v-model="input"/>
+            <button type="button" @click="addlist">Submit</button>
         </form>
         <ul class="list">
             <li v-for="(item,index) in list" :key="index">
-                {{item.title}} <span class="delete" @click="del">Hapus</span>
+                {{item.title}} <span class="delete" @click="deletelist(index)">Hapus</span>
             </li>
         </ul>
     </div>
@@ -18,17 +18,31 @@ export default ({
     data() {
         return{
             list:[
-                  {
+                {
                     title:"menyapu"
                 },
                 {
                     title:"menyapu"
                 }
             ],
-            Input:""
+            input:""
         
         }
               
     },
+
+    methods:{
+        addlist(){
+            let newlist = {
+                title: this.input
+            }
+            this.list.push(newlist)
+            this.input = ""
+        },
+
+        deletelist(index){
+            this.list.splice(index,1)
+        }
+    }
 })
 </script>
